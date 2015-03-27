9edce37891aeacaf7b2477641a849109e6a2f322
Lucas <lucas@agtlucas.com>
2015-03-27 10:42:04 -0300

    Updates

--- index.ios.js
+++ index.ios.js
@@ -9,6 +9,10 @@ var React = require('react-native');
 var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
 var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
 
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+
 var MOCKED_MOVIES_DATA = [
   {
     title: 'Title',
@@ -28,8 +32,40 @@ var {
 } = React;
 
 var NativeReact = React.createClass({
+  getInitialState: function () {
+    return { movies: null };
+  },
+  componentDidMount: function () {
+    this.fetchData();
+  },
+  fetchData: function () {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
   render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  },
+
+  renderLoadingView: function () {
+    return (
+      <View style={styles.container}>
+        <Text>Loading movies...</Text>
+      </View>
+    );
+  },
+
+  renderMovie: function (movie) {
     return (
       <View style={styles.container}>
         <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
@@ -38,7 +74,7 @@ var NativeReact = React.createClass({
           <Text style={styles.year}>{movie.year}</Text>
         </View>
       </View>
-    );
+    )
   }
 });
 
